ced4dcc6f4cb7ace929737dbfccd2e60bb28c033
SANCHIT JAIN <sanchit@cse.iitb.ac.in>
2017-09-28 14:25:13 +0530

    with splash screen

--- app/src/main/java/iitbombay/code_catalyst/com/insti20/MainActivity.java
+++ app/src/main/java/iitbombay/code_catalyst/com/insti20/MainActivity.java
@@ -2,6 +2,7 @@ package iitbombay.code_catalyst.com.insti20;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
