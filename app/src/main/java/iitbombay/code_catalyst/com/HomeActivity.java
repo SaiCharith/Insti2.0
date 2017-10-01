acfdb396d367be037d41ffec5e037080760721ed
SANCHIT JAIN <sanchit@cse.iitb.ac.in>
2017-10-02 01:23:41 +0530

    mess secy choice implemented partially

---
+++ app/src/main/java/iitbombay/code_catalyst/com/HomeActivity.java
@@ -0,0 +1,15 @@
+package iitbombay.code_catalyst.com;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import iitbombay.code_catalyst.com.insti20.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
