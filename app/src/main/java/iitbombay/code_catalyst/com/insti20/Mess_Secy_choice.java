4295081d5b6e200a7a70d21bfff7d7cb38c6bcf1
SANCHIT JAIN <sanchit@cse.iitb.ac.in>
2017-10-25 15:36:19 +0530

    Mess_secy_choice documented

--- app/src/main/java/iitbombay/code_catalyst/com/insti20/Mess_Secy_choice.java
+++ app/src/main/java/iitbombay/code_catalyst/com/insti20/Mess_Secy_choice.java
@@ -6,13 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+/**
+ * It is the activity which lets the mess secy to choose whether he wants to go to the original app or wants to update the menu
+ */
 public class Mess_Secy_choice extends AppCompatActivity {
-
+    /**
+     * create two objects of type button and adds a required listener that intents to the the corresponding activity
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mess__secy_choice);
+        /**
+         * Button for secy to go to update menu interface
+         */
         Button update_menu= (Button) findViewById(R.id.updatemenu);
+        /**
+         * Button for secy to go to Original app i.e. {@link After_login}
+         */
         Button original_app=(Button) findViewById(R.id.originalapp);
         original_app.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
